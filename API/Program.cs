60fdf0f3f146d363379f862ee9e6ae26483c3375
bhatia0301 <namanbhatia0301@gmail.com>
2026-05-05 01:12:26 +0530

    Setup the angular UI and configure CORS

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); 
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +23,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+                .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
